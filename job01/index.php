68314d7a34ecc4c34546cb1e0ae1f4b69b9e1867
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-15 16:53:32 +0200

    dossier job01 et fichier index.php

---
+++ job01/index.php
@@ -0,0 +1,21 @@
+<?php
+$str = " La plateforme";
+$str2 = "Vive ";
+$str3 = "!";
+//afficher: "Vive laPlateforme !"
+$result = $str2.$str.$str3;
+echo $result;
+
+//ajouter 4 à une variable
+$val = 6;
+echo $val;
+$val = $val + 4;
+echo $val;
+
+//changer la variable booleeene en false
+$myBool = true;
+echo $myBool;
+$myBool = false;
+echo $myBool;
+
+?>
