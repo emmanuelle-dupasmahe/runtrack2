1e61d5ac12e71ab9fed5d9c199c453fdec15b477
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-16 15:26:18 +0200

    création de job07 pour construire un triangle

---
+++ jour02/job07/index.php
@@ -0,0 +1,14 @@
+<?php
+//Faire un algorithme qui affiche un triangle de 5 de hauteur. Cette dimension devra être
+//stockée dans une variable $hauteur, modifiable facilement.
+$hauteur = 5;
+for ($i = 1; $i <= $hauteur; $i++) {
+    for ($j = 1; $j <= $hauteur - $i; $j++) {
+        echo "&nbsp;&nbsp;"; // Un espace insécable pour un bon alignement
+    }
+    for ($k = 1; $k <= (2 * $i) - 1; $k++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+?>
