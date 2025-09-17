388bb2574c470bc7b0a7ecb2751da5ae4d4f9cfd
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-17 15:30:22 +0200

    job01 tableau nombres pairs et impaires

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -1,2 +1,9 @@
 <?php
 $numbers =[200, 204, 173, 98, 171, 404, 459];
+    foreach ($numbers as $number) {
+    if ($number % 2 == 0) {
+        echo $number . " est paire."."<br>";
+    } else {
+        echo $number . " est impaire."."<br>";
+    }
+}
