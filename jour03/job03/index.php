cbfaf16a01000774e860f4be34ce5feebbf3e586
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-17 16:11:08 +0200

    changement de mon fichier index.php qui affiche uniquement les voyelles dans job03 en n'utilisant uniquement la fonction asset

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -1,9 +1,14 @@
 <?php
 $str = "I'm sorry Dave I'm afraid I can't do that.";
-$voyelles = "aeiouyAEIOUY";
+$voyelles = ["a" => true, "e" => true, "i" => true, "o" => true, "u" => true, "y" => true, "A" => true, "E" => true, "I" => true, "O" => true, "U" => true, "Y" => true];
 
-for ($i = 0; $i < strlen($str); $i++) {
-    if (strpos($voyelles, $str[$i]) !== false) {
-        echo $str[$i];
+$i = 0;
+while (isset($str[$i])) {
+    $caractere = $str[$i];
+    if (isset($voyelles[$caractere])) {
+        echo $caractere;
     }
+    $i++;
 }
+
+
