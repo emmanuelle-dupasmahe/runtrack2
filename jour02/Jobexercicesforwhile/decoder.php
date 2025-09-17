45811946bacf0ffeb96bd0e938b3893de4cc671b
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-17 10:44:38 +0200

    création du dossier jobexerciceforwhile du fichier entrainement.php et decoder.php

---
+++ jour02/Jobexercicesforwhile/decoder.php
@@ -0,0 +1,7 @@
+<?php
+/*"Coder tous les jours, un petit pas à la fois"*/
+
+$str = "Coder tous les jours, un petit pas à la fois";
+for ($i = 1; $i <= 12; $i++) {
+    echo "Séance n°" . $i . " terminée\n"."<br>"."<br>";
+}
---
+++ jour02/Jobexercicesforwhile/entrainement.php
@@ -0,0 +1,17 @@
+<?php
+/* ."<br>"."<br>" Affiche les 10 premiers nombres pairs.*/ 
+for ($i = 1; $i <= 10; $i++) {
+    $pair = $i * 2;
+    echo "Nombre pair : $pair <br>"."<br>"."<br>";
+}
+
+/*Crée un fichier entrainement.php. Écris un script qui affiche :
+
+Séance n°1 terminée  
+Séance n°2 terminée  
+... jusqu’à Séance n°12 terminée*/ 
+
+for ($i = 1; $i <= 12; $i++) {
+    echo "Séance n°" . $i . " terminée\n";
+}
+
