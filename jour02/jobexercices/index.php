b2a23912e5f5c6406dda40d81d86a51a68d36709
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-17 09:49:59 +0200

    fin des exercices sur switch et match

--- jour02/jobexercices/index.php
+++ jour02/jobexercices/index.php
@@ -51,7 +51,7 @@ switch ($jour) {
     case "mardi":
     case "mercredi":
     case "jeudi":
-    case"vendredi":
+    case "vendredi":
         echo "Ouvert 10h - 19h"."<br>"."<br>";
         break;
 
@@ -82,4 +82,122 @@ switch ($transport) {
     
     default:
         echo "Ce mode de transport n'est pas gérer par le réseau Mistral."."<br>"."<br>";
+}
+
+//Résultat RCT (match)
+//$resultat ∈ {"Victoire", "Défaite", "Nul"} → message adapté.résultat RCT (match)
+
+$résultat = "Victoire";
+
+$message = match ($résultat) {
+    "Victoire" => "Toulon vainqueur !" ."<br>"."<br>",
+    "Défaite" => "Toulon a perdu !"."<br>"."<br>",
+    "Nul" => "Match nul !"."<br>"."<br>",
+    default => " ? "."<br>"."<br>",
+};
+
+echo $message;
+
+//Quai + heure (switch + echo)
+//Selon $destination (3 cases), affiche aussi un horaire indicatif (ex. "Départ toutes les 20 min").
+$destination = "Six-Fours";
+switch ($destination) {
+    case "Six-Fours":
+        echo "C'est le quai n°2 à 13H07. Départ toutes les heures."."<br>"."<br>";
+        break;
+    
+    case "Toulon":
+        echo "C'est le quai n°3 à 13H23. Départ toutes les 20 minutes."."<br>"."<br>";
+        break;
+    
+    case "Paris":
+        echo "C'est le quai n°1 voie E à 11h04. Départ imminent."."<br>"."<br>";
+        break;
+
+    default:
+        echo "Cette destination n'est pas desservie."."<br>"."<br>";
+}
+
+/*Réduction à Mayol (match)
+$carte ∈ {"Étudiant", "Senior"} → 10% / 15% ; défaut 0%.*/
+
+$carte = "Senior";
+
+$message = match ($carte) {
+    "Etudiant" => "Vous avez une réduction de 10%." ."<br>"."<br>",
+    "Senior" => "Vous avez une réduction de 15%."."<br>"."<br>",
+    default => " Plein Tarif "."<br>"."<br>",
+};
+echo $message;
+
+/*Météo Toulon (switch)
+$meteo ∈ {"Soleil", "Pluie", "Vent"} → message adapté (plage du Mourillon, coder au centre, attention au Mistral).*/
+
+$meteo = "Vent";
+
+switch ($meteo) {
+    case "Vent":
+        echo "plage du Mourillon, attention au Mistral "."<br>"."<br>";
+        break;
+    
+    case "Soleil":
+        echo "journée ensoleillée."."<br>"."<br>";
+        break;
+    
+    case "Pluie":
+        echo "pluie prenez vos parapluie."."<br>"."<br>";
+        break;
+
+    default:
+        echo "belle journée."."<br>"."<br>";
+}
+/*Billet RCT (match)
+$categorie ∈ {"Enfant", "Adulte", "VIP"} → prix 10 / 20 / 50 ; affiche "Prix du billet : X €".*/
+
+$categorie = "Adulte";
+$message = match ($categorie) {
+    "Enfant" => "Prix du billet : 10 €."."<br>"."<br>",
+    "Adulte" => "Prix du billet : 20 €."."<br>"."<br>",
+    "VIP" => "Prix du billet : 50 €."."<br>"."<br>",
+    default =>"Prix du billet : 20 €."."<br>"."<br>", 
+};
+echo $message;
+
+/*Jours de formation DWWM (switch)
+Lundi–Vendredi → "Cours DWWM" ; Samedi/Dimanche → "Repos".*/
+
+$jour = "lundi";
+switch ($jour) {
+    case "lundi":
+    case "mardi":
+    case "mercredi":
+    case "jeudi":
+    case "vendredi":
+        echo "Cours DWWM."."<br>"."<br>";
+        break;
+    case "samedi":
+    case "dimanche":
+        echo "Repos."."<br>"."<br>";
+        break;
+    default:
+        echo "Jour invalide"."<br>"."<br>";
+}
+
+/*Itinéraire rapide (switch)
+$ligne ∈ {"Bus 23", "Bus 40", "Bateau"} → affiche la destination (Cap Brun, Mourillon, La Seyne).*/ 
+
+$ligne = "Bateau";
+switch ($ligne) {
+    case "Bus 23":
+        echo "Destination le Cap Brun."."<br>"."<br>";
+        break;
+    case "Bus 40":
+        echo "Destination le Mourillon."."<br>"."<br>";
+        break;
+    case  "Bateau":
+        echo "Destination La Seyne."."<br>"."<br>";
+        break;
+    default :
+    echo "cette ligne de bus n'est pas répertoriée.";
+
 }
