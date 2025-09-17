fca7c6d1dbe0ef71bb8fe75540fac0af75ecea7b
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-17 16:48:31 +0200

    revoir ne fonctionne pas exactement comme prevu

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -1,2 +1,17 @@
 <?php
 $str = "Certaines choses changent, et d'autres ne changeront jamais.";
+$length = 0;
+
+while (isset($str[$length])) {
+    $length++;
+}
+$lastChar = $str[$length - 1]; 
+
+for ($i = $length - 1; $i > 0; $i--) {
+    $str[$i] = $str[$i - 1];
+}
+
+$str[0] = $lastChar;
+
+echo $str;
+?>
