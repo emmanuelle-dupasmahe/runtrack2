94d36a7655fc0ea10e8ef1ba54417c693e507d9e
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-18 09:36:51 +0200

    fichier qui donne la moyenne des notes d'un tableau de 4 étudiants

--- jour02/jobexercices_tableaux/etudiants_moyennes.php
+++ jour02/jobexercices_tableaux/etudiants_moyennes.php
@@ -3,3 +3,18 @@ $etudiants = [["nom"=>"Lina","notes"=>[14,16,12]],
 ["nom"=>"Jeremy","notes"=>[13,11,16]],
 ["nom"=>"Alphonse","notes"=>[9,10,17]],
 ["nom"=>"Margotte","notes"=>[12,19,17]]];
+
+foreach ($etudiants as $etudiant) {
+    $somme_notes = 0;
+    $compteur_notes = 0;
+
+    foreach ($etudiant["notes"] as $note) {
+        $somme_notes += $note;
+        $compteur_notes++;
+    }
+
+    $moyenne = $somme_notes / $compteur_notes;
+    $etudiant["moyenne"] = $moyenne;
+    
+    echo "Nom : " . $etudiant["nom"] . " - Moyenne : " . $etudiant["moyenne"] . "<br>";
+}
