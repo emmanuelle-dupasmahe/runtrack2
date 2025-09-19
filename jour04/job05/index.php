28eb1aed35467e11314e62e43d067fb81f010112
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-19 09:54:01 +0200

    programme qui demande le user et le mot de passe donc pas d'utilisation de $-GET pour ne pas afficher le mot de passe dans l'url.

---
+++ jour04/job05/index.php
@@ -0,0 +1,24 @@
+<!-- login.php -->
+<form action="index.php" method="post">
+  <label>Utilisateur :</label>
+  <input type="text" name="username"><br>
+  
+  <label>Mot de passe :</label>
+  <input type="password" name="password"><br>
+  
+  <input type="submit" value="Se connecter">
+</form>
+
+<?php
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+
+    if ($user === "John" && $pass === "Rambo") {
+        echo "<b>Ce n'est pas ma guerre $user ! 💣️ </b>";
+    } else {
+        echo "<b>Votre pire cauchemar. 🦂</b>";
+    }
+} 
+?>
