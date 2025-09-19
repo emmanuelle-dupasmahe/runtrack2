7884526ff3e9ff0e67b24c8aa16317c9bd3d41eb
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-19 16:35:44 +0200

    création du programme qui change un email en majuscule en minuscule.

---
+++ jour04/exercices_supplementaires/exo8_email.php
@@ -0,0 +1,6 @@
+<!--Formulaire demandant un email. Affiche-le en minuscules.-->
+<form method="post">
+  <input type="text" name="email" placeholder="Entrer votre Email">
+  <input type="submit" value="Envoyer">
+</form>
+<?php
