9ed28b936dae81b9b752193133501558e60b8a2d
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-19 10:15:27 +0200

    suite

--- jour04/job06/index.php
+++ jour04/job06/index.php
@@ -1,6 +1,6 @@
 <!-- login.php -->
  <h2> Veuiller entrer un nombre : </h2>
-<form action="index.php" method="post">
+<form action="index.php" method="get">
   <label>Nombre :</label>
   <input type="text" name="number"><br>
   <input type="submit" value="Envoyer">
@@ -8,12 +8,12 @@
 
 <?php
 
-if (isset($_POST['number'])) {
-    $nombre = $_POST['number'];
+if (isset($_GET['number'])) {
+    $nombre = $_GET['number'];
     if ($nombre % 2 == 0) {
         echo "Nombre pair : " . $nombre;
     } else {
-        echo "Nombre impair :" .$nombre;
+        echo "Nombre impair : " .$nombre;
     }
 } 
 ?>
