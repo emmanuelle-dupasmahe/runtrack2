de68e765ade5dd6e3a29783b4be24c8ec0780f8b
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-19 16:42:10 +0200

    suite

--- jour04/exercices_supplementaires/exo8_email.php
+++ jour04/exercices_supplementaires/exo8_email.php
@@ -3,4 +3,21 @@
   <input type="text" name="email" placeholder="Entrer votre Email">
   <input type="submit" value="Envoyer">
 </form>
+
 <?php
+if (isset($_POST['submit']) && isset($_POST['email'])) {
+    $email = $_POST['email'];
+    
+
+    if ($email !== '') {
+
+        $majuscules = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $minuscules = 'abcdefghijklmnopqrstuvwxyz';
+        
+        
+        echo "Votre e-mail en minuscules est : " . ($email_minuscules);
+    } else {
+        echo "Veuillez entrer une adresse e-mail.";
+    }
+}
+?>
