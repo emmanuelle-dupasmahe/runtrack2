14d9b6f21aba17a09b6ac61f15dc790000c1e0f3
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-22 12:04:57 +0200

    J'ai changé pour utiliser que la fonction isset au lieu de strlen et strpos

--- jour02/Jobexercicesforwhile/filtre_de_type.php
+++ jour02/Jobexercicesforwhile/filtre_de_type.php
@@ -1,12 +1,22 @@
 <?php
-/*Filtre de type : parcours une chaîne 
+/*Filtre de type : parcours une chaîne
 et affiche uniquement les lettres (ignore espaces et ponctuation).*/
 
 $str = "J'ai faim ! Puis-je manger ? Oui, on dirait.";
 $car_a_ignorer = " .,?/;!";
 
-for ($i = 0; $i < strlen($str); $i++) {
-    if (strpos($car_a_ignorer, $str[$i]) === false) {
+
+for ($i = 0; isset($str[$i]); $i++) {
+    $est_a_ignorer = false;
+    // Boucle pour vérifier chaque caractère à ignorer
+    for ($j = 0; isset($car_a_ignorer[$j]); $j++) {
+        if ($str[$i] === $car_a_ignorer[$j]) {
+            $est_a_ignorer = true;
+            break; 
+        }
+    }
+    // Si le caractère n'est pas dans la liste des caractères à ignorer, on l'affiche
+    if (!$est_a_ignorer) {
         echo $str[$i];
     }
 }
