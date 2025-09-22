7e49f19bea16d2b7bfe0368a11673eb9de5df02a
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-22 09:30:11 +0200

    Conversion d'un mail en majuscule en minuscule.

--- jour04/exercices_supplementaires/exo8_email.php
+++ jour04/exercices_supplementaires/exo8_email.php
@@ -5,19 +5,38 @@
 </form>
 
 <?php
-if (isset($_POST['submit']) && isset($_POST['email'])) {
-    $email = $_POST['email'];
-    
+if (isset($_POST['email']) && ($_POST['email'])!=="") {
 
-    if ($email !== '') {
+    $email_entree = $_POST['email'];
 
-        $majuscules = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $minuscules = 'abcdefghijklmnopqrstuvwxyz';
-        
-        
-        echo "Votre e-mail en minuscules est : " . ($email_minuscules);
-    } else {
-        echo "Veuillez entrer une adresse e-mail.";
+    $longueur_email = 0;
+    while (isset($email_entree[$longueur_email])) {
+        $longueur_email++;
     }
+
+    $conversion_table = array(
+        'A' => 'a', 'B' => 'b', 'C' => 'c', 'D' => 'd', 'E' => 'e', 'F' => 'f', 'G' => 'g',
+        'H' => 'h', 'I' => 'i', 'J' => 'j', 'K' => 'k', 'L' => 'l', 'M' => 'm', 'N' => 'n',
+        'O' => 'o', 'P' => 'p', 'Q' => 'q', 'R' => 'r', 'S' => 's', 'T' => 't', 'U' => 'u',
+        'V' => 'v', 'W' => 'w', 'X' => 'x', 'Y' => 'y', 'Z' => 'z'
+    );
+
+    $email_minuscules = '';
+
+    for ($i = 0; $i < $longueur_email; $i++) {
+        $caractere = $email_entree[$i];
+
+        //  si il y a une majuscule
+        if (isset($conversion_table[$caractere])) {
+            $email_minuscules .= $conversion_table[$caractere];
+        } else {
+            $email_minuscules .= $caractere;
+        }
+    }
+
+    echo "L'email converti est : " . $email_minuscules;
+
+} else {
+    echo "Veuillez entrer un email.";
 }
 ?>
