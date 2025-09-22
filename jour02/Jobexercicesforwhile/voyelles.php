df44384052b54bcadbc2e21488436bdc942aadee
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-22 12:09:43 +0200

    J'ai changé pour utiliser que la fonction isset au lieu de strlen et strpos

--- jour02/Jobexercicesforwhile/voyelles.php
+++ jour02/Jobexercicesforwhile/voyelles.php
@@ -2,9 +2,15 @@
 $str = "Apprendre PHP demande de la pratique";
 $voyelles = "aeiouyAEIOUY";
 
-for ($i = 0; $i < strlen($str); $i++) {
-    if (strpos($voyelles, $str[$i]) !== false) {
-        echo $str[$i];
+
+for ($i = 0; isset($str[$i]); $i++) {
+    
+    for ($j = 0; isset($voyelles[$j]); $j++) {
+        // Si le caractère de la chaîne correspond à une voyelle
+        if ($str[$i] === $voyelles[$j]) {
+            echo $str[$i];
+            break; // On sort de la boucle interne
+        }
     }
 }
 ?>
