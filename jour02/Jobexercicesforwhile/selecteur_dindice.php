ac8ea2f7bc478f4a341aff3d5f74e6e2db03d0af
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-22 12:07:08 +0200

    J'ai changé pour utiliser que la fonction isset au lieu de strlen

--- jour02/Jobexercicesforwhile/selecteur_dindice.php
+++ jour02/Jobexercicesforwhile/selecteur_dindice.php
@@ -3,10 +3,11 @@
 //Sélecteur d’indices : affiche les indices pairs d’une chaîne donnée.
 $str = "Je commence à avoir faim !";
 $resultat = "";
-for ($i = 0; $i < strlen($str); $i++) {
+for ($i = 0; isset($str[$i]); $i++) {
     if ($i % 2 == 0) {
         $resultat .= $str[$i];
     }
 }
 echo $resultat;
+
 ?>
