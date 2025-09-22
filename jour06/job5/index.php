a49a4464e057e258c24664ce62e35ed7967ade95
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-22 11:18:31 +0200

    création de 4 programmes 1 php et 3 fichier style1.css, style2.css et style3.css

---
+++ jour06/job5/index.php
@@ -0,0 +1,44 @@
+<!--Formulaire qui permet de choisir un style.css-->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Changement de Style</title>
+    
+    <?php
+    if (isset($_GET['choix_style'])) {
+        $choix_style = $_GET['choix_style'];
+        $style_a_appliquer = 'css/default.css'; // Style par défaut
+
+        switch ($choix_style) {
+            case 'style1':
+                $style_a_appliquer = 'style1.css';
+                break;
+            case 'style2':
+                $style_a_appliquer = 'style2.css';
+                break;
+            case 'style3':
+                $style_a_appliquer = 'style3.css';
+                break;
+        }
+        echo '<link rel="stylesheet" href="' . $style_a_appliquer . '">';
+    }
+    ?>
+</head>
+<body>
+
+    <h3>Choisissez un style.</h3>
+    <form method="get">
+        <label for="choix_style">Choisissez un style:</label>
+        <select name="choix_style" id="choix_style">
+            <option value="style1">style1</option>
+            <option value="style2">style2</option>
+            <option value="style3">style3</option>
+        </select>
+        <br><br>
+        <input type="submit" name="submit" value="Envoyer">
+    </form>
+    
+    </body>
+</html>
